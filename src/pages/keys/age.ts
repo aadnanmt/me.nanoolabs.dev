efe86c8349225e32ec612570b8c14ae84fd18d8a
Adnan Slamet Wibowo <adnan@nanoolabs.dev>
2026-08-04 14:37:42 +0700

    fix: update age public key

--- src/pages/keys/age.ts
+++ src/pages/keys/age.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro"
-const ageKey = "age1ac229rf6czjc595p24s553e7p3vy9taht7m4dfw3k7yhenhzh5ts8rermw"
+const ageKey = "age15lcnlpxv8t6qqt82rcjcagx0h9sx85jdjhj5x3frqaa9u675x56qusmsgp"
 export const GET: APIRoute = () => {
   return new Response(ageKey, {
     headers: {
